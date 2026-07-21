6328ed77b43041d535e81d2ff6e8d090a83781a8
NGUYEN THI ANH TUYET <tuyet.nguyenthianh.bbs23@eiu.edu.vn>
2026-07-21 22:38:54 +0700

    Add files via upload

--- MIS 443_2332300334_School/codes/01_create_database.sql
+++ MIS 443_2332300334_School/codes/01_create_database.sql
@@ -1,1 +1,14 @@
+/*
+========================================================
+MIS 443 - Business Data Management
+Assignment 4 - School Database
 
+Author: Nguyen Thi Anh Tuyet
+Student ID: 2332300334
+
+File: 01_create_database.sql
+Purpose: Create the database.
+========================================================
+*/
+
+CREATE DATABASE practice;
